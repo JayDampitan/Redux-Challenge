5a3a1e23e6bd6ff7e31d6cf3fd72a0315bf31ce6
JayDampitan <jaykennethpd@gmail.com>
2021-09-17 11:34:45 -0700

    feat(order-time): change order time to the correct format of hh:mm:ss and add month, date and year ordered.

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 const OrdersList = (props) => {
     const { orders } = props;
-    if (!props || !props.orders || !props.orders.length) return (
+    if ( !props ||!props.orders || !orders.length) return (
         <div className="empty-orders">
             <h2>There are no orders to display</h2>
         </div>
     );
 
     return orders.map(order => {
+
         const createdDate = new Date(order.createdAt);
+        
+        const formattedDate = (dateObject) => {
+            const time = {
+                date: dateObject.getDate(),
+                month: dateObject.getMonth() + 1,
+                year: dateObject.getFullYear(),
+                hours: dateObject.getHours().toString().padStart(2, "0"),
+                minutes: dateObject.getMinutes().toString().padStart(2, "0"),
+                seconds: dateObject.getSeconds().toString().padStart(2, "0"),
+            };
+            return `${time.month}/${time.date}/${time.year} ${time.hours}:${time.minutes}:${time.seconds}`
+        }
+
+        const orderedDate = formattedDate(createdDate);
+    
         return (
             <div className="row view-order-container" key={order._id}>
                 <div className="col-md-4 view-order-left-col p-3">
@@ -17,7 +33,7 @@ const OrdersList = (props) => {
                     <p>Ordered by: {order.ordered_by || ''}</p>
                 </div>
                 <div className="col-md-4 d-flex view-order-middle-col">
-                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
+                    <p>Order placed at {orderedDate}</p>
                     <p>Quantity: {order.quantity}</p>
                 </div>
                 <div className="col-md-4 view-order-right-col">
